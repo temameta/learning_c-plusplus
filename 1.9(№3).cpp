b1fb07381fca351f986f3c4dd701e018e2134381
temameta <62208316+temameta@users.noreply.github.com>
2022-11-21 19:36:09 +0300

    Create 1.9(№3).cpp

---
+++ 1.9(№3).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, fak = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++) fak*=i;
+    cout << fak << endl;
+    return 0;
+}

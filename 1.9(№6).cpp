c63588a24420ae5d13bfa9170fb71eb7f6750328
temameta <62208316+temameta@users.noreply.github.com>
2022-11-21 19:37:21 +0300

    Create 1.9(№6).cpp

---
+++ 1.9(№6).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 0; i <= n; i++) {
+        if (i%3==0) sum+=i;
+    }
+    cout << sum << endl;
+    return 0;
+}

ea8e6e286cec4164cd8e7eae0e286ca8d69638e8
temameta <62208316+temameta@users.noreply.github.com>
2022-11-21 19:14:37 +0300

    Create 1.8(№1).cpp

---
+++ 1.8(№1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+    cin >> n;
+    while (count!=n) {
+        cout << "programming";
+        count++;
+    }
+    return 0;
+}

0e0f0630c8a2c345ab3380e12185cc48c8644cff
temameta <62208316+temameta@users.noreply.github.com>
2022-11-21 19:15:02 +0300

    Create 1.8(№2).cpp

---
+++ 1.8(№2).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0, sum_chet = 0;
+    do {
+        cin >> n;
+        if (n%2==0) {
+            sum_chet+=n;
+        }
+        sum+=n;
+    } while(sum_chet<50);
+    cout << sum << "\n" << sum_chet;
+    return 0;
+}

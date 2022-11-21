ad34f00784fe6eccc95a1d8d909be4199d8eeb5c
temameta <62208316+temameta@users.noreply.github.com>
2022-11-21 19:43:07 +0300

    Create 1.8(№7).cpp

---
+++ 1.8(№7).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, popytka_count = 0;
+    do
+    {
+        popytka_count++;
+        cout << "Сколько будет 2+ 2?" << endl;
+        cin >> num;
+        if (num == 4) break;
+        cout << "Нет! Попробуй еще раз!" << endl;
+    } while (num != 4);
+    cout << "Правильно!" << "\n" << popytka_count << endl;
+    return 0;
+}

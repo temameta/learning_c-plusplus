46f773b7173b16beaf0cf0c777a0085ef0aa4bf1
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 20:17:26 +0300

    Create 1.11(№1).cpp

---
+++ 1.11(№1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[3][3] {};
+    
+    for (int i = 0; i<3; i++) {
+        for (int j = 0; j<3; j++) {
+            arr[i][j] = i*3+j+1;
+            printf("%2d",arr[i][j]);
+        }
+        cout << endl;
+    }
+    return 0;
+}

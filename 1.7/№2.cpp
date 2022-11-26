f066b6936af986e219c3e46c6ee6b353d5f5ca62
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 20:36:17 +0300

    Update and rename 1.7(№2).cpp to 1.7/№2.cpp

--- 1.7/№2.cpp
+++ 1.7/№2.cpp
@@ -5,13 +5,16 @@ using namespace std;
 int main()
 {
     int hero;
+    
     cout << "Выбери героя\n 1. Иван-Царевич\n 2. Илья Муромец\n 3. Добрыня Никитич\n 4. Алёша Попович" << endl;
     cin >> hero;
+    
     switch (hero) {
         case 1: cout << "Кощей Бессмертный" << endl; break;
         case 2: cout << "Соловей-разбойник" << endl; break;
         case 3: cout << "Змей Горыныч" << endl; break;
         case 4: cout << "Тугарин Змей" << endl; break;
     }
+    
     return 0;
 }

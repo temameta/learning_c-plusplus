b587d0753e19cc844b363f7912f683f83d0ee369
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:13:47 +0300

    Update and rename 1.9(№3).cpp to 1.9/№3.cpp

--- 1.9/№3.cpp
+++ 1.9/№3.cpp
@@ -6,7 +6,8 @@ int main()
 {
     int n, fak = 1;
     cin >> n;
-    for (int i = 1; i <= n; i++) fak*=i;
+    for (int i = 1; i <= n; i++) 
+        fak*=i;
     cout << fak << endl;
     return 0;
 }

7c54079f2b44ad4bb81237969e077c02852a9f62
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:15:36 +0300

    Update and rename 1.9(№6).cpp to 1.9/№6.cpp

--- 1.9/№6.cpp
+++ 1.9/№6.cpp
@@ -6,9 +6,8 @@ int main()
 {
     int n, sum = 0;
     cin >> n;
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++)
         if (i%3==0) sum+=i;
-    }
     cout << sum << endl;
     return 0;
 }

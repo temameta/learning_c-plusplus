077692890f652e4132093d6396a185db5cd29351
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:01:37 +0300

    Update and rename 1.10(№6).cpp to 1.10/№6.cpp

--- 1.10/№6.cpp
+++ 1.10/№6.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main()
 {
     int arr[] {9,8,7,6,5,4,3,2,1,0}, count = 0;
-    for (int i = 0; i<(sizeof(arr)/sizeof(arr[0])); i++) {
+    
+    for (int i = 0; i<(sizeof(arr)/sizeof(arr[0])); i++)
         if (arr[i] % 3 == 0) count++;
-    }
+    
     cout << count;
     return 0;
 }

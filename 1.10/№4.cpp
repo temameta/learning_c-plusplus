f0fa842c841abfa3f9da9436f30bf1eb4c1a8dfc
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:00:45 +0300

    Update and rename 1.10(№4).cpp to 1.10/№4.cpp

--- 1.10/№4.cpp
+++ 1.10/№4.cpp
@@ -5,9 +5,10 @@ int main()
 {
     int arr[] {9,8,7,6,5,4,3,2,1,0};
     int sum = 0;
-    for (int i = 0; i<(sizeof(arr)/sizeof(arr[0])); i++) {
+    
+    for (int i = 0; i<(sizeof(arr)/sizeof(arr[0])); i++)
         if (i % 2 == 0) sum+=arr[i];
-    }
+    
     cout << sum;
     return 0;
 }

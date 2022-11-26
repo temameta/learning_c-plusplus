b22619a61fc02cc7c1a6dc37373bd52e5c51e762
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:12:50 +0300

    Update and rename 1.9(№1).cpp to 1.9/№1.cpp

--- 1.9/№1.cpp
+++ 1.9/№1.cpp
@@ -6,6 +6,7 @@ int main()
 {
     int n;
     cin >> n;
-    for (int i = 0; i <= n; i++) cout << "programming" << endl;
+    for (int i = 0; i <= n; i++) 
+        cout << "programming" << endl;
     return 0;
 }

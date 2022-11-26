0c0f3d656602c7de873e12445b582d0e200ee71d
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 20:36:53 +0300

    Update and rename 1.8(№1).cpp to 1.8/№1.cpp

--- 1.8/№1.cpp
+++ 1.8/№1.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
     int n, count = 0;
+    
     cin >> n;
+    
     while (count!=n) {
         cout << "programming";
         count++;
     }
+    
     return 0;
 }

4d08d744927ca26f11bdafe8503afc370c744c48
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:06:56 +0300

    Update and rename 1.8(№5).cpp to 1.8/№5.cpp

--- 1.8/№5.cpp
+++ 1.8/№5.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int main()
 {
     int num, krat5_count = 0;
-    do
-    {
+    do {
         cin >> num;
         if (num!=0 && num%3==0 && num%10==5)
-        {
             krat5_count++;
-        }
     } while (num != 0);
     cout << krat5_count;
     return 0;

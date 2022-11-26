3261977ade05cd31db9bb9cd32e4d30cee995072
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 20:40:28 +0300

    Update and rename 1.8(№4).cpp to 1.8/№4.cpp

--- 1.8/№4.cpp
+++ 1.8/№4.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main()
 {
     int n, delitel = 0;
+    
     cin >> n;
+    
     while(delitel!= n) {
         delitel++;
-        if(n%delitel==0) {
-            cout << delitel << endl;
-        }
+        if(n%delitel==0)
+            cout << delitel << endl; 
     }
     return 0;
 }

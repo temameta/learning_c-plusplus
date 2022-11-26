cf6dac855b096df84638416f53b52ea13c6a273a
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 21:05:31 +0300

    Update and rename 1.8(№3).cpp to 1.8/№3.cpp

--- 1.8/№3.cpp
+++ 1.8/№3.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int main()
 {
     int num, chet_count = 0;
-    do
-    {
+    do {
         cin >> num;
         if (num!=0 && num%2==0)
-        {
             chet_count++;
-        }
     } while (num != 0);
     cout << chet_count;
     return 0;

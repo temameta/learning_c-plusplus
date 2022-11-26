d37aab9bb2ff4d7f6ea3009fda9a2b5accac8726
temameta <62208316+temameta@users.noreply.github.com>
2022-11-26 20:37:56 +0300

    Update and rename 1.8(№2).cpp to 1.8/№2.cpp

--- 1.8/№2.cpp
+++ 1.8/№2.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main()
 {
     int n, sum = 0, sum_chet = 0;
+    
     do {
         cin >> n;
-        if (n%2==0) {
+        if (n%2==0)
             sum_chet+=n;
-        }
         sum+=n;
     } while(sum_chet<50);
+    
     cout << sum << "\n" << sum_chet;
     return 0;
 }
